2a1f9245bcbed904739ccc767380302af67043ff
seun2230 <seun2230@gmail.com>
2022-01-10 12:15:01 +0900

    Common Module Isolation

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector(' input');
-
-// input 검색란을 focus하는 함수 작성
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  // 특정요소에 클래스 정보를 가진 객체에 추가함
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-// focus가 해제되었을 때 (blur)
-searchInputEl.addEventListener('blur', function() {
-  // 특정요소에 클래스 정보를 가진 객체에 추가함
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-// 오류 해결: remove를 안하고 blur를 하였더니 검색란이 blur되지 않았음
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector("#to-top");
 // 화면 자체에 스크롤을 추가하면 해당 함수를 출력한다.
@@ -163,8 +139,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') 
     .addTo(new ScrollMagic.Controller());     
 })
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); 
-console.log("Year test", thisYear.textContent);
